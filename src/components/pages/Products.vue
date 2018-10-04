8bc29b0ee6f1b80a1519a37d0f7946e297e004d9
Molly_Lin <mollylin2014@gmail.com>
2018-10-04 13:48:19 +0800

    產品列表的分頁邏輯

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -33,6 +33,27 @@
                 </tr>
             </tbody>
         </table>
+
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item" :class="{'disabled':!pagination.has_pre }">
+                <a class="page-link" href="#" aria-label="Previous" @click.prevent="getProducts(pagination.current_page - 1)">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                </li>
+                <li class="page-item" v-for="page in pagination.total_pages" :key="page" :class="{'active':pagination.current_page === page}">
+                    <a class="page-link" href="#" @click.prevent="getProducts(page)">1</a>
+                </li>
+                <li class="page-item" :class="{'disabled': !pagination.has_nex}">
+                    <a class="page-link" href="#" aria-label="Next" @click.prevent="getProducts(pagination.current_page +1)">
+                        <span aria-hidden="true">&raquo;</span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
         <div class="modal fade" id="productModal">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
@@ -156,6 +177,7 @@
         data () {
             return {
                 products: [],
+                pagination: {},
                 tempProduct: {},
                 isNew: false,
                 isLoading: false, //方法一:畫面讀取效果 使用 vue-loading-overlay
@@ -165,14 +187,15 @@
             }
         },
         methods: {
-            getProducts () {
-                const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/products`;
+            getProducts (page = 1) {
+                const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/products?page=${page}`;
                 const vm = this;
                 vm.isLoading = true;
                 this.$http.get(api).then((response) => {
                     console.log(response.data);
                     vm.products = response.data.products;
                     vm.isLoading = false;
+                    vm.pagination = response.data.pagination;
                 })
             },
             openModal(isNew, item) {
